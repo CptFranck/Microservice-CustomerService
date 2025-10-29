5951a15cdbc33ea7ebaa6e9c2e0fcbe1d1b0cef7
CptFranck <francois.caillet71@gmail.com>
2025-10-29 14:55:21 +0100

    add CustomerEntity

---
+++ src/main/java/com/CptFranck/CustomerService/entity/CustomerEntity.java
@@ -0,0 +1,32 @@
+package com.CptFranck.CustomerService.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "customer")
+public class CustomerEntity {
+
+
+    @Id
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "address")
+    private String address;
+}
