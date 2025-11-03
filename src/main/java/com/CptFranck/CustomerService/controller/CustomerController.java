20d8fdfe26d3e2be707c591e151630acaa2d9396
CptFranck <francois.caillet71@gmail.com>
2025-11-03 17:26:01 +0100

    update CustomerController

--- src/main/java/com/CptFranck/CustomerService/controller/CustomerController.java
+++ src/main/java/com/CptFranck/CustomerService/controller/CustomerController.java
@@ -1,11 +1,11 @@
 package com.CptFranck.CustomerService.controller;
 
-import com.CptFranck.CustomerService.dto.CustomerResponse;
 import com.CptFranck.CustomerService.service.CustomerService;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.CptFranck.dto.CustomerDto;
+import com.CptFranck.dto.KeycloakUserDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1")
@@ -17,8 +17,53 @@ public class CustomerController {
         this.customerService = customerService;
     }
 
-    @PostMapping(consumes = "application/json", produces = "application/json", path = "/customer")
-    public CustomerResponse createBooking(JwtAuthenticationToken auth ){
-        return customerService.getOrCreateUser(auth);
+    @PostMapping(consumes = "application/json", produces = "application/json", path = "/customers")
+    public ResponseEntity<CustomerDto> createCustomer(@RequestBody KeycloakUserDto request) {
+        try {
+            CustomerDto customer = customerService.create(
+                    Long.valueOf(request.getKeycloakId()),
+                    request.getUsername(),
+                    request.getEmail()
+            );
+            return ResponseEntity.status(HttpStatus.CREATED).body(customer);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(null);
+        }
     }
+
+    @PutMapping(consumes = "application/json",produces = "application/json", path = "/customers")
+    public ResponseEntity<CustomerDto> updateCustomer(@RequestBody CustomerDto customerDto) {
+        try {
+            CustomerDto customer = customerService.update(
+                    customerDto.getId(),
+                    customerDto.getUsername(),
+                    customerDto.getEmail(),
+                    customerDto.getAddress()
+            );
+            return ResponseEntity.ok(customer);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+
+    @DeleteMapping("/{keycloakId}")
+    public ResponseEntity<CustomerDto> deleteCustomer(@PathVariable Long keycloakId) {
+        try {
+            CustomerDto customer = customerService.delete(keycloakId);
+            return ResponseEntity.ok(customer);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+//    @GetMapping("/me")
+//    public UserInfoDto getGretting(JwtAuthenticationToken auth) {
+//        return new UserInfoDto(
+//                auth.getToken().getClaimAsString(StandardClaimNames.PREFERRED_USERNAME),
+//                auth.getAuthorities().stream().map(GrantedAuthority::getAuthority).toList());
+//    }
+
+//    @GetMapping(path = "/customer")
+//    public CustomerResponse createBooking(JwtAuthenticationToken auth){
+//        return customerService.getOrCreateUser(auth);
+//    }
 }
