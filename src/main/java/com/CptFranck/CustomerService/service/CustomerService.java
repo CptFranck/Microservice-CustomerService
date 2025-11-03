d27515715ac20b96002b9e6d31213c25787baed8
CptFranck <francois.caillet71@gmail.com>
2025-11-03 16:56:28 +0100

    update CustomerService

--- src/main/java/com/CptFranck/CustomerService/service/CustomerService.java
+++ src/main/java/com/CptFranck/CustomerService/service/CustomerService.java
@@ -1,13 +1,9 @@
 package com.CptFranck.CustomerService.service;
 
-import com.CptFranck.CustomerService.dto.CustomerResponse;
 import com.CptFranck.CustomerService.entity.CustomerEntity;
 import com.CptFranck.CustomerService.repository.CustomerRepository;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
-
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 
 @Service
 @Slf4j
@@ -19,23 +15,48 @@ public class CustomerService {
         this.customerRepository = customerRepository;
     }
 
-    public CustomerResponse getOrCreateUser(JwtAuthenticationToken auth) {
-        final Jwt jwt = auth.getToken();
+    public CustomerEntity create(Long keycloakId, String name, String email) {
+        if (customerRepository.existsById(keycloakId))
+            throw new IllegalArgumentException("Customer already exists with id: " + keycloakId);
 
-        String keycloakId = jwt.getClaimAsString("sub");
-        String username = jwt.getClaimAsString("preferred_username");
-        String email = jwt.getClaimAsString("email");
+        CustomerEntity customer = new CustomerEntity(keycloakId, name, email, "");
 
-        Long id = Long.valueOf(keycloakId);
-
-        CustomerEntity customer = customerRepository.findById(id)
-                .orElseGet(() -> customerRepository.save(new CustomerEntity(id, username, email, "")));
-
-        return CustomerResponse.builder()
-                .customerId(customer.getId())
-                .name(customer.getName())
-                .email(customer.getEmail())
-                .address(customer.getAddress())
-                .build();
+        return customerRepository.save(customer);
     }
+
+    public CustomerEntity update(Long keycloakId, String name, String email, String address) {
+        CustomerEntity customer = customerRepository.findById(keycloakId)
+                .orElseThrow(() -> new IllegalArgumentException("Customer not found with id: " + keycloakId));
+
+        customer.setName(name);
+        customer.setEmail(email);
+        customer.setAddress(address);
+        return customerRepository.save(customer);
+    }
+
+    public void deleteCustomer(Long keycloakId) {
+        CustomerEntity customer = customerRepository.findById(keycloakId)
+                .orElseThrow(() -> new IllegalArgumentException("Customer not found with id: " + keycloakId));
+        customerRepository.delete(customer);
+    }
+
+//    public CustomerResponse getOrCreateUser(JwtAuthenticationToken auth) {
+//        final Jwt jwt = auth.getToken();
+//
+//        String keycloakId = jwt.getClaimAsString("sub");
+//        String username = jwt.getClaimAsString("preferred_username");
+//        String email = jwt.getClaimAsString("email");
+//
+//        Long id = Long.valueOf(keycloakId);
+//
+//        CustomerEntity customer = customerRepository.findById(id)
+//                .orElseGet(() -> customerRepository.save(new CustomerEntity(id, username, email, "")));
+//
+//        return CustomerResponse.builder()
+//                .customerId(customer.getId())
+//                .name(customer.getName())
+//                .email(customer.getEmail())
+//                .address(customer.getAddress())
+//                .build();
+//    }
 }
