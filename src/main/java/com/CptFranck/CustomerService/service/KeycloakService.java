ad180ce88adccf6a84d137192b35ec8838eac9f8
CptFranck <francois.caillet71@gmail.com>
2025-11-12 16:21:57 +0100

    update CustomerService

---
+++ src/main/java/com/CptFranck/CustomerService/service/KeycloakService.java
@@ -0,0 +1,40 @@
+package com.CptFranck.CustomerService.service;
+
+import lombok.RequiredArgsConstructor;
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.KeycloakBuilder;
+import org.keycloak.representations.idm.UserRepresentation;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class KeycloakService {
+
+    @Value("${keycloak.auth-server-url}")
+    private String serverURL;
+    @Value("${keycloak.realm}")
+    private String realm;
+    @Value("${keycloak.resource}")
+    private String clientId;
+    @Value("${keycloak.credentials.secret}")
+    private String clientSecret;
+
+    private final Keycloak keycloakAdminClient;
+
+    public KeycloakService() {
+        this.keycloakAdminClient = KeycloakBuilder.builder()
+                .serverUrl(serverURL)
+                .realm(realm)
+                .clientId(clientId)
+                .clientSecret(clientSecret)
+                .grantType("client_credentials")
+                .build();
+    }
+
+    public List<UserRepresentation> getAllUsers() {
+         return keycloakAdminClient.realm(realm).users().list();
+    }
+}
