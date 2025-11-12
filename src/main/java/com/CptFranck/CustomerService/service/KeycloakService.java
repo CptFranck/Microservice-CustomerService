d31450a31221930951943e5c02f7ef27e254ba6b
CptFranck <francois.caillet71@gmail.com>
2025-11-12 16:54:29 +0100

    update KeycloakService

--- src/main/java/com/CptFranck/CustomerService/service/KeycloakService.java
+++ src/main/java/com/CptFranck/CustomerService/service/KeycloakService.java
@@ -1,9 +1,6 @@
 package com.CptFranck.CustomerService.service;
 
-import lombok.RequiredArgsConstructor;
-import org.keycloak.OAuth2Constants;
 import org.keycloak.admin.client.Keycloak;
-import org.keycloak.admin.client.KeycloakBuilder;
 import org.keycloak.representations.idm.UserRepresentation;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -11,28 +8,15 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-@RequiredArgsConstructor
 public class KeycloakService {
 
-    @Value("${keycloak.auth-server-url}")
-    private String serverURL;
     @Value("${keycloak.realm}")
     private String realm;
-    @Value("${keycloak.resource}")
-    private String clientId;
-    @Value("${keycloak.credentials.secret}")
-    private String clientSecret;
 
     private final Keycloak keycloakAdminClient;
 
-    public KeycloakService() {
-        this.keycloakAdminClient = KeycloakBuilder.builder()
-                .serverUrl(serverURL)
-                .realm(realm)
-                .clientId(clientId)
-                .clientSecret(clientSecret)
-                .grantType(OAuth2Constants.CLIENT_CREDENTIALS)
-                .build();
+    public KeycloakService(Keycloak keycloakAdminClient) {
+        this.keycloakAdminClient = keycloakAdminClient;
     }
 
     public List<UserRepresentation> getAllUsers() {
