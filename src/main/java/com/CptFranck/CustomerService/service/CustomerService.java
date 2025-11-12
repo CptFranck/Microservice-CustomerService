6b9f976bc9c7a4d44738125f9c890563083bab49
CptFranck <francois.caillet71@gmail.com>
2025-11-12 16:28:11 +0100

    add UnregisteredKeycloakUser CustomerService

--- src/main/java/com/CptFranck/CustomerService/service/CustomerService.java
+++ src/main/java/com/CptFranck/CustomerService/service/CustomerService.java
@@ -4,16 +4,25 @@ import com.CptFranck.CustomerService.entity.CustomerEntity;
 import com.CptFranck.CustomerService.repository.CustomerRepository;
 import com.CptFranck.dto.CustomerDto;
 import lombok.extern.slf4j.Slf4j;
+import org.keycloak.representations.idm.UserRepresentation;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
 public class CustomerService {
 
+    private final KeycloakService keycloakService;
+
     private final CustomerRepository customerRepository;
 
-    public CustomerService(CustomerRepository customerRepository) {
+    public CustomerService(CustomerRepository customerRepository, KeycloakService keycloakService) {
         this.customerRepository = customerRepository;
+        this.keycloakService = keycloakService;
     }
 
     public CustomerDto createFromKeycloakUser(String keycloakId, String username, String email, String firstname, String lastname) {
@@ -66,6 +75,31 @@ public class CustomerService {
                 .lastname(customer.getLastname())
                 .address(customer.getAddress())
                 .build();
+    }
+
+    @Scheduled(fixedRateString = "${keycloak.scheduled.mapping.fixedRate}", initialDelayString = "${keycloak.scheduled.mapping.initialDelay}")
+    public void UnregisteredKeycloakUser() {
+        try {
+            List<UserRepresentation> users = keycloakService.getAllUsers();
+            Set<String> customersId = customerRepository.findAll().stream()
+                    .map(CustomerEntity::getId)
+                    .collect(Collectors.toSet());
+
+            for (UserRepresentation user : users) {
+                if (!customersId.contains(user.getId())) {
+                    createFromKeycloakUser(
+                            user.getId(),
+                            user.getUsername(),
+                            user.getEmail(),
+                            user.getFirstName(),
+                            user.getLastName()
+                    );
+                }
+            }
+            log.info("✅ Synchronisation Keycloak terminée : {} utilisateurs vérifiés", users.size());
+        } catch (Exception e) {
+            log.error("❌ Erreur pendant la synchronisation Keycloak", e);
+        }
     }
 
 //    public CustomerResponse getOrCreateUser(JwtAuthenticationToken auth) {
