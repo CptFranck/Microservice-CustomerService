ef9802a81fa578377bd010d31085708898db5709
CptFranck <francois.caillet71@gmail.com>
2025-11-12 16:54:16 +0100

    add KeycloakConfig

---
+++ src/main/java/com/CptFranck/CustomerService/config/KeycloakConfig.java
@@ -0,0 +1,46 @@
+package com.CptFranck.CustomerService.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.keycloak.OAuth2Constants;
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.KeycloakBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+public class KeycloakConfig {
+
+    @Value("${keycloak.auth-server-url}")
+    private String serverUrl;
+
+    @Value("${keycloak.realm}")
+    private String realm;
+
+    @Value("${keycloak.resource}")
+    private String clientId;
+
+    @Value("${keycloak.credentials.secret}")
+    private String clientSecret;
+
+    @Bean
+    public Keycloak keycloakAdminClient() {
+
+        log.info("🔑 Initialisation du client Keycloak pour le serverUrl {}", serverUrl);
+        log.info("🔑 Initialisation du client Keycloak pour le realm {}", realm);
+
+        return KeycloakBuilder.builder()
+                .serverUrl(serverUrl)
+                .realm(realm)
+                .clientId(clientId)
+                .clientSecret(clientSecret)
+                .grantType(OAuth2Constants.CLIENT_CREDENTIALS)
+                .build();
+    }
+
+    @Bean
+    public String keycloakRealm() {
+        return realm;
+    }
+}
