1f8ba6dedb6fed9775cf55fe2d3d41399e3437cd
CptFranck <francois.caillet71@gmail.com>
2025-11-13 14:43:18 +0100

    remove log from KeycloakConfig

--- src/main/java/com/CptFranck/CustomerService/config/KeycloakConfig.java
+++ src/main/java/com/CptFranck/CustomerService/config/KeycloakConfig.java
@@ -1,6 +1,5 @@
 package com.CptFranck.CustomerService.config;
 
-import lombok.extern.slf4j.Slf4j;
 import org.keycloak.OAuth2Constants;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.admin.client.KeycloakBuilder;
@@ -8,7 +7,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Slf4j
 @Configuration
 public class KeycloakConfig {
 
@@ -26,10 +24,6 @@ public class KeycloakConfig {
 
     @Bean
     public Keycloak keycloakAdminClient() {
-
-        log.info("🔑 Initialisation du client Keycloak pour le serverUrl {}", serverUrl);
-        log.info("🔑 Initialisation du client Keycloak pour le realm {}", realm);
-
         return KeycloakBuilder.builder()
                 .serverUrl(serverUrl)
                 .realm(realm)
